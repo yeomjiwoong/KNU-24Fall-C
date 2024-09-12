924add0b4996eeb5c3060f4c833a6e48eb0cd26a
yeomjiwoong <yeomjiwoong6632@gmail.com>
2024-09-12 16:23:05 +0900

    조건문 실습

--- day02/day02-1.c
+++ day02/day02-1.c
@@ -1,10 +1,22 @@
 #include<stdio.h>
 int main()
 {
-	float a, b;
+	int opr;
+	printf("연산타입 : ");
+	scanf_s("%d", &opr);
+
+	double num1, num2, num3, num4;
+	printf("첫번째 숫자:");
+	scanf_s("%lf", &num1);
+	printf("두번째 숫자: ");
+	scanf_s("%lf", &num2);
+	
+
+	double
 
 	printf("원하는 기능을 입력하시오.\n");
 
 	printf("1.더하기 2.뺴기 3. 곱하기 4.나누기\n");
 
+	printf("기능 : ")
 }
